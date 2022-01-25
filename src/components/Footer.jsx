ce39f497d63569c91319e944fe09a24bcf7a7765
Eddie <eddiesr93@gmail.com>
2022-01-25 14:57:40 +0200

    Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,130 @@
+import styled from "styled-components";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import RoomIcon from "@mui/icons-material/Room";
+import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
+import MailIcon from "@mui/icons-material/Mail";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+const Logo = styled.h1``;
+
+const Description = styled.p`
+  margin: 20px 0;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: #fff;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>STORE.</Logo>
+        <Description>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur
+          hic recusandae ullam repellendus, iste saepe enim earum itaque.
+          Impedit ad illum tenetur placeat, deleniti perferendis quod quisquam
+          veritatis quam quis.
+        </Description>
+        <SocialContainer>
+          <SocialIcon color="3b5999">
+            <FacebookIcon />
+          </SocialIcon>
+          <SocialIcon color="e4405f">
+            <InstagramIcon />
+          </SocialIcon>
+          <SocialIcon color="55acee">
+            <TwitterIcon />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <RoomIcon style={{ marginRight: "10px" }} /> Bucharest, Romania
+        </ContactItem>
+        <ContactItem>
+          <PhoneIphoneIcon style={{ marginRight: "10px" }} /> +4070123456
+        </ContactItem>
+        <ContactItem>
+          <MailIcon style={{ marginRight: "10px" }} /> contact@store.ro
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
